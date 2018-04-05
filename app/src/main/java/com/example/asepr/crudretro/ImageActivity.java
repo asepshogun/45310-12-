2b0148449f77b6ddef37ce298fb18ed66a2c66bc
aseprizal <aseprizal70@gmail.com>
2018-04-05 19:37:22 +0700

    ubah dikit take picture, masih bug

--- app/src/main/java/com/example/asepr/crudretro/ImageActivity.java
+++ app/src/main/java/com/example/asepr/crudretro/ImageActivity.java
@@ -8,6 +8,7 @@ import android.database.Cursor;
 import android.graphics.Bitmap;
 import android.net.Uri;
 import android.os.Environment;
+import android.os.StrictMode;
 import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -44,6 +45,7 @@ public class ImageActivity extends AppCompatActivity {
     ApiInterface mApiInterfaceImg, mApiInterface;
     Intent intent = new Intent();
     ProgressDialog progressDialog;
+    private Uri mMediaUri;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -67,11 +69,12 @@ public class ImageActivity extends AppCompatActivity {
         btCam.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                StrictMode.VmPolicy.Builder builder = new StrictMode.VmPolicy.Builder();
+                StrictMode.setVmPolicy(builder.build());
                 intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-                Uri file = Uri.fromFile(getOutputMediaFile());
-                intent.putExtra(MediaStore.EXTRA_OUTPUT, file);
+                mMediaUri = Uri.fromFile(getOutputMediaFile());
+                intent.putExtra(MediaStore.EXTRA_OUTPUT, mMediaUri);
                 startActivityForResult(intent, 2);
-
             }
         });
         btBrowse.setOnClickListener(new View.OnClickListener() {
@@ -80,7 +83,6 @@ public class ImageActivity extends AppCompatActivity {
                 //intent.setType("image/*");
                 //intent.setAction(Intent.ACTION_GET_CONTENT);
                 //startActivityForResult(Intent.createChooser(intent, "Select Picture"),1);
-
                 Intent i = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
                 startActivityForResult(i, 1);
             }
@@ -96,13 +98,14 @@ public class ImageActivity extends AppCompatActivity {
                     photo = MediaStore.Images.Media.getBitmap(getContentResolver(), data.getData());
                     imageView.setImageBitmap(photo);
                     Uri selectedImage = data.getData();
-                    uploadFile(selectedImage);
+                    uploadFile(selectedImage, 1);
                     break;
                 case 2:
                     if (resultCode == Activity.RESULT_OK) {
                         /*photo = (Bitmap) data.getExtras().get("data");
                         imageView.setImageBitmap(photo);*/
-
+                        imageView.setImageURI(mMediaUri);
+                        uploadFile(mMediaUri,2);
                     } else if (resultCode == Activity.RESULT_CANCELED) {
                         Toast.makeText(getApplicationContext(), "Selecting picture cancelled", Toast.LENGTH_LONG).show();
                     }
@@ -125,39 +128,67 @@ public class ImageActivity extends AppCompatActivity {
         }
 
         String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
-        return new File(mediaStorageDir.getPath() + File.separator +
+        File file = new File(mediaStorageDir.getPath() + File.separator +
                 "IMG_"+ timeStamp + ".jpg");
+        return (file);
     }
 
     // Uploading Image/Video
-    private void uploadFile(Uri fileUri) {
+    private void uploadFile(Uri fileUri, int type) {
         progressDialog.show();
         // Map is used to multipart the file using okhttp3.RequestBody
-        File file = new File(getRealPathFromURI(fileUri));
-        RequestBody requestBody = RequestBody.create(MediaType.parse("**/"), file);
-        MultipartBody.Part fileToUpload = MultipartBody.Part.createFormData("fileToUpload", file.getName(), requestBody);
-        RequestBody token = RequestBody.create(MediaType.parse("token"), "52f866f58d909e13236110e5");
-        RequestBody project = RequestBody.create(MediaType.parse("project"), "api_andro");
+        if(type == 1){
+            File file = new File(getRealPathFromURI(fileUri));
+            RequestBody requestBody = RequestBody.create(MediaType.parse("**/"), file);
+            MultipartBody.Part fileToUpload = MultipartBody.Part.createFormData("fileToUpload", file.getName(), requestBody);
+            RequestBody token = RequestBody.create(MediaType.parse("token"), "52f866f58d909e13236110e5");
+            RequestBody project = RequestBody.create(MediaType.parse("project"), "api_andro");
+            mApiInterfaceImg = ApiClient.getClientImg().create(ApiInterface.class);
+            Call<Img> call = mApiInterfaceImg.postImage(fileToUpload, token, project);
 
-        mApiInterfaceImg = ApiClient.getClientImg().create(ApiInterface.class);
-        Call<Img> call = mApiInterfaceImg.postImage(fileToUpload, token, project);
+            call.enqueue(new Callback<Img>() {
+                @Override
+                public void onResponse(Call<Img> call, Response<Img> response) {
+                    if(response.body().getMessage().equals("Success")){
+                        loadImageData(response.body().getFilename());
+                    }else{
+                        Toast.makeText(getApplicationContext(), "Image isn't saved", Toast.LENGTH_LONG).show();
+                    }progressDialog.dismiss();
+                }
 
-        call.enqueue(new Callback<Img>() {
-            @Override
-            public void onResponse(Call<Img> call, Response<Img> response) {
-                if(response.body().getMessage().equals("Success")){
-                    loadImageData(response.body().getFilename());
-                }else{
-                    Toast.makeText(getApplicationContext(), "Image isn't saved", Toast.LENGTH_LONG).show();
-                }progressDialog.dismiss();
-            }
+                @Override
+                public void onFailure(Call<Img> call, Throwable t) {
+                    Log.e("Keshav","onUpload image onFailure Method called");
+                    progressDialog.dismiss();
+                }
+            });
+        }else if(type == 2){
+            File file = new File(String.valueOf(fileUri));
+            /*File file = new File(getRealPathFromURI(fileUri));*/
+            RequestBody requestBody = RequestBody.create(MediaType.parse("**/"), file);
+            MultipartBody.Part fileToUpload = MultipartBody.Part.createFormData("fileToUpload", file.getName(), requestBody);
+            RequestBody token = RequestBody.create(MediaType.parse("token"), "52f866f58d909e13236110e5");
+            RequestBody project = RequestBody.create(MediaType.parse("project"), "api_andro");
+            mApiInterfaceImg = ApiClient.getClientImg().create(ApiInterface.class);
+            Call<Img> call = mApiInterfaceImg.postImage(fileToUpload, token, project);
 
-            @Override
-            public void onFailure(Call<Img> call, Throwable t) {
-                Log.e("Keshav","onUpload image onFailure Method called");
-                progressDialog.dismiss();
-            }
-        });
+            call.enqueue(new Callback<Img>() {
+                @Override
+                public void onResponse(Call<Img> call, Response<Img> response) {
+                    if(response.body().getMessage().equals("Success")){
+                        loadImageData(response.body().getFilename());
+                    }else{
+                        Toast.makeText(getApplicationContext(), "Image isn't saved", Toast.LENGTH_LONG).show();
+                    }progressDialog.dismiss();
+                }
+
+                @Override
+                public void onFailure(Call<Img> call, Throwable t) {
+                    Log.e("Keshav","onUpload image onFailure Method called");
+                    progressDialog.dismiss();
+                }
+            });
+        }
     }
 
     private String getRealPathFromURI(Uri contentUri) {
